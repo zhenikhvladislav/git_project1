5918088ccb88e2714b92eb1012991a4a0751b371
zhenikhvladislav <zhenikhvladislav@gmail.com>
2024-12-03 20:43:28 +0300

    Изменил комментарий (Владислав Жених)

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    print('Я изменил свой комментарий')
 
 
 if __name__ == '__main__':
