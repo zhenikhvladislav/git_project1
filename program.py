e177de22e5a920dfa5c97e782708d882909d3d3a
zhenikhvladislav <zhenikhvladislav@gmail.com>
2024-12-03 20:52:27 +0300

    Удалил лишние принты(Влад)

--- program.py
+++ program.py
@@ -1,9 +1,6 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
-    print('Добавил строку')
 
 
 if __name__ == '__main__':
